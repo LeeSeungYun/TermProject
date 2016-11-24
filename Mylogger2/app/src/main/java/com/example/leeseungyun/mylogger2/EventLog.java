484bb1df39bf290b3cc5470599e38a0c8c0f5cbb
lee seungyun <seungyun0324@gmail.com>
2016-11-25 03:12:57 +0900

    레이아웃1차 구현
    메인,이벤트 입력,지도 레이아웃 구현

---
+++ Mylogger2/app/src/main/java/com/example/leeseungyun/mylogger2/EventLog.java
@@ -0,0 +1,14 @@
+package com.example.leeseungyun.mylogger2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Lee Seung Yun on 2016-11-24.
+ */
+public class EventLog extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventlog);
+    }
+}
---
+++ Mylogger2/app/src/main/java/com/example/leeseungyun/mylogger2/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.leeseungyun.mylogger2;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
+public class MainActivity extends Activity {
+    public GetGPS GPS ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkDangerousPermissions();
+
+        Button eventlogBtn = (Button) findViewById(R.id.logEvent);
+        Button viewmapBtn = (Button) findViewById(R.id.viewMap);
+        Button statisticBtn = (Button) findViewById(R.id.statistic);
+        Button goalBtn = (Button) findViewById(R.id.goal);
+        Button resetBtn = (Button) findViewById(R.id.reset);
+
+        eventlogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(),EventLog.class);
+                startActivity(it);
+            }
+        });
+
+        viewmapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(),MapView.class);
+                startActivity(it);
+            }
+        });
+
+        statisticBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(),Statistics.class);
+                startActivity(it);
+            }
+        });
+
+        goalBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(),MapView.class);
+                startActivity(it);
+            }
+        });
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //데이터 베이스 삭제
+                Toast.makeText(getApplicationContext(),"모두 삭제되었습니다.",LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    private void checkDangerousPermissions(){
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for(int i=0 ; i< permissions.length; i++){
+            permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
+            if(permissionCheck == PackageManager.PERMISSION_DENIED){
+                break;
+            }
+        }
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, "권한 있음", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(this, "권한 없음", Toast.LENGTH_LONG).show();
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,permissions[0])){
+                Toast.makeText(this,"권한 설명 필요함.", Toast.LENGTH_LONG).show();
+            }else{
+                ActivityCompat.requestPermissions(this,permissions,1);
+            }
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if(requestCode == 1){
+            for(int i=0 ; i<permissions.length ; i++){
+                if(grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, permissions[i] + "권한이 승인됨.", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(this,permissions[i] + "권한이 승인되지 않음.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+}
